b637033dcf9873f15e3d4133b409673bb6c39ee3
MatiasGonzalez89 <matiasagonzalez89@gmail.com>
2022-06-17 13:05:03 -0300

    Creando componente NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,22 @@
+
+
+const NavBar = () => {
+  return (
+    <nav>
+      <div class="nav-wrapper #4a148c purple darken-4">
+        <a href="#" class="brand-logo">E-commerce</a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+          <li><a href="#">Inicio</a></li>
+          <li><a href="#">Productos</a></li>
+          <li><a href="#">Contacto</a></li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
+
+export default NavBar;
+
+
+
+
