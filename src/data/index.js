3f11d58ab29bd73b55737f90620af93250c35e80
MatiasGonzalez89 <matiasagonzalez89@gmail.com>
2022-07-08 20:36:29 -0300

    cambio imagenes

--- src/data/index.js
+++ src/data/index.js
@@ -21,7 +21,7 @@ const productos = [
         id: 3,
         title: "Remera Tres",
         color: "Rojo",
-        imgUrl: "https://netivooregon.s3.amazonaws.com/attach/modelo/20220311/1318/34544323.jpg?5425",
+        imgUrl: "https://netivooregon.s3.amazonaws.com/attach/modelo/20220216/2146/55548159.jpg?10046",
         description:"Remera de algodon color rojo",
         price: "1500",
         stock: "3"
@@ -30,7 +30,7 @@ const productos = [
         id: 4,
         title: "Remera Cuatro",
         color: "Azul",
-        imgUrl: "https://netivooregon.s3.amazonaws.com/attach/modelo/20220311/1318/34544323.jpg?5425",
+        imgUrl: "https://netivooregon.s3.amazonaws.com/attach/modelo/20220304/2146/72138124.jpg?10046",
         description:"Remera de algodon color azul",
         price: "1500",
         stock: "3"
@@ -39,7 +39,7 @@ const productos = [
         id: 5,
         title: "Remera Cinco",
         color: "Amarillo",
-        imgUrl: "https://netivooregon.s3.amazonaws.com/attach/modelo/20220311/1318/34544323.jpg?5425",
+        imgUrl: "https://netivooregon.s3.amazonaws.com/attach/modelo/20220414/2265/70994087.jpg?22571",
         description:"Remera de algodon color amarillo",
         price: "1500",
         stock: "4"
